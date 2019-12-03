6148cebf0ff8b1d0692f1f02cca4477a49ff8b81
ryota.arase <ryota.arase@i-studio.co.jp>
2019-12-03 19:34:15 +0900

    sapperの環境構築
    プリプロセッサ系も設定

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,15 @@
+<template lang="pug">
+  .content
+    Nav({segment})
+
+    main
+      slot()
+</template>
+
+<script>
+  import Nav from '../components/Nav.svelte'
+
+  export let segment
+</script>
+
+<style src="./__styles/layout.styl"></style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<template lang="pug">
+  h1 Great success!
+
+  figure
+    img(alt='Borat' src='great-success.png')
+    figcaption タイピング
+
+  p
+    strong Try editing this file (src/routes/index.svelte) to test live reloading.
+</template>
+
+<style src="./__styles/index.styl"></style>
